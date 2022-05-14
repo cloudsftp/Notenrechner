8dfbbe4cd190f434d6c71c2416409fe8053bde41
Fabian Weik <fabian.weik@protonmail.com>
2022-05-14 12:04:07 +0200

    change gui framework to giu

--- src/gui.go
+++ src/gui.go
@@ -1,36 +1,30 @@
 package main
 
 import (
-	"log"
-	"os"
+	"fmt"
 
-	"github.com/asticode/go-astikit"
-	"github.com/asticode/go-astilectron"
+	g "github.com/AllenDang/giu"
 )
 
+func onClickMe() {
+	fmt.Println("Hello world!")
+}
+
+func onImSoCute() {
+	fmt.Println("Im sooooooo cute!!")
+}
+
+func loop() {
+	g.SingleWindow().Layout(
+		g.Label("Hello world from giu"),
+		g.Row(
+			g.Button("Click Me").OnClick(onClickMe),
+			g.Button("I'm so cute").OnClick(onImSoCute),
+		),
+	)
+}
+
 func main() {
-	// Initialize astilectron
-	var a, _ = astilectron.New(log.New(os.Stderr, "", 0), astilectron.Options{
-		AppName:            "Notenrechner",
-		AppIconDefaultPath: "", // If path is relative, it must be relative to the data directory
-		AppIconDarwinPath:  "", // Same here
-		BaseDirectoryPath:  "Deps",
-		VersionAstilectron: "0.55.0",
-		VersionElectron:    "18.2.3",
-	})
-	defer a.Close()
-
-	// Start astilectron
-	a.Start()
-
-	// Create a new window
-	var w, _ = a.NewWindow("http://127.0.0.1:4000", &astilectron.WindowOptions{
-		Center: astikit.BoolPtr(true),
-		Height: astikit.IntPtr(600),
-		Width:  astikit.IntPtr(600),
-	})
-	w.Create()
-
-	// Blocking pattern
-	a.Wait()
+	wnd := g.NewMasterWindow("Hello world", 400, 200, g.MasterWindowFlagsNotResizable)
+	wnd.Run(loop)
 }
